aa9fdd562c2d1c13bf95ab338b6a172e1c63513f
Elpuma20 <joseluisrodri540@gmail.com>
2026-06-05 17:00:25 -0400

    V6.0

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: import.meta.env.PROD
-        ? 'https://edulogica-backend-05p9.onrender.com/api'
+        ? 'https://backend-edulogica.onrender.com/api'
         : 'http://localhost:8000/api',
 });
 
