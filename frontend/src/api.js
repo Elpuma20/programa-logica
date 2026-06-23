0afbbc2be81a51b6f3eea27c896a3f69aa68969d
Elpuma20 <joseluisrodri540@gmail.com>
2026-06-23 10:50:35 -0400

    Fix localhost IPv6 issue for local dev

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL || (import.meta.env.PROD
         ? 'https://api.matematicalogica.dev/api'
-        : 'http://localhost:8000/api'),
+        : 'http://127.0.0.1:8000/api'),
 });
 
 export default api;
